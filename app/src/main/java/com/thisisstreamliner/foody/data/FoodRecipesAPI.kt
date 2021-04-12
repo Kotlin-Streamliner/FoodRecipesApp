842b3965abd9235c4adf8bde71af229a945b2b00
Tiger <thisisstreamliner@gmail.com>
2021-04-12 15:44:18 +0300

    Add FoodRecipesAPI interface

---
+++ app/src/main/java/com/thisisstreamliner/foody/data/FoodRecipesAPI.kt
@@ -0,0 +1,17 @@
+package com.thisisstreamliner.foody.data
+
+import com.thisisstreamliner.foody.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesAPI {
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+
+
+
+}
