e5dbc3c55ba5771587880526dde78d9dc4014b3e
Tiger <thisisstreamliner@gmail.com>
2021-04-13 17:34:16 +0300

    Inject RemoteDataSource

--- app/src/main/java/com/thisisstreamliner/foody/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/thisisstreamliner/foody/data/remote/RemoteDataSource.kt
@@ -1,4 +1,11 @@
 package com.thisisstreamliner.foody.data.remote
 
-class RemoteDataSource {
+import com.thisisstreamliner.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesAPI: FoodRecipesAPI
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> = foodRecipesAPI.getRecipes(queries)
 }
