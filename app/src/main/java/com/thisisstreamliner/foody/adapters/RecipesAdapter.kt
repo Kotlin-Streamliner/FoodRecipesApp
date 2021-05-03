c4baabe168f5a980f55ae2d8087aac7d1508d9eb
thisisstreamliner <thisisstreamliner@gmail.com>
2021-05-03 15:39:21 +0300

    Create RecipesRowBinding.

--- app/src/main/java/com/thisisstreamliner/foody/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/thisisstreamliner/foody/adapters/RecipesAdapter.kt
@@ -10,21 +10,21 @@ import com.thisisstreamliner.foody.util.RecipesDiffUtil
 
 class RecipesAdapter: RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
 
-    private var recipe = emptyList<Result>()
+    private var recipes = emptyList<Result>()
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentResult = recipe[position]
-        holder.bind(currentResult)
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
     }
 
     fun setData(newData: FoodRecipe) {
-        val recipesDiffUtil = RecipesDiffUtil(recipe, newData.results)
+        val recipesDiffUtil = RecipesDiffUtil(recipes, newData.results)
         val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
-        recipe = newData.results
+        recipes = newData.results
         diffUtilResult.dispatchUpdatesTo(this)
     }
 
-    override fun getItemCount(): Int = recipe.size
+    override fun getItemCount(): Int = recipes.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder = MyViewHolder.from(parent)
 
