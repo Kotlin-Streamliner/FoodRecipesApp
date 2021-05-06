e9e2405d5e8444875e967b1ea488ad380c962390
thisisstreamliner <thisisstreamliner@gmail.com>
2021-05-06 15:33:09 +0300

    Create DAO (ROOM)

---
+++ app/src/main/java/com/thisisstreamliner/foody/data/local/RecipesDAO.kt
@@ -0,0 +1,17 @@
+package com.thisisstreamliner.foody.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipesDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @Query("SELECT * FROM RECIPES_TABLE ORDER BY id ASC")
+    fun readRecipes(): Flow<List<RecipesEntity>>
+}
