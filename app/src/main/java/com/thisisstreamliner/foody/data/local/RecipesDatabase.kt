be0039449528612e2982fa82a161618310a5e14c
thisisstreamliner <thisisstreamliner@gmail.com>
2021-05-12 12:34:29 +0300

    Create RecipesDatabase

---
+++ app/src/main/java/com/thisisstreamliner/foody/data/local/RecipesDatabase.kt
@@ -0,0 +1,15 @@
+package com.thisisstreamliner.foody.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [RecipesEntity::class],
+        version = 1,
+        exportSchema = false
+)
+@TypeConverters(RecipesTypeConverter::class)
+abstract class RecipesDatabase: RoomDatabase() {
+
+    abstract fun recipesDao(): RecipesDAO
+}
